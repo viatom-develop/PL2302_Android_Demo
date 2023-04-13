ddbd5b3f53a78ecb34e76688ea842fa582335017
liguanghao <liguanghao@viatomtech.com>
2023-04-13 16:17:36 +0800

    want service3

--- app/src/main/java/com/example/pl2302_android/MainActivity.kt
+++ app/src/main/java/com/example/pl2302_android/MainActivity.kt
@@ -1,233 +1,220 @@
-package com.example.pl2302_android;
-import tw.com.prolific.pl2303gmultilib.PL2303GMultiLib;
-import android.os.Bundle;
-import android.os.Handler;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.hardware.usb.UsbManager;
-import android.os.Looper;
-import android.util.Log;
-import android.widget.Toast;
+package com.example.pl2302_android
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity
+import tw.com.prolific.pl2303gmultilib.PL2303GMultiLib
+import com.example.pl2302_android.uart.UARTSettingInfo
+import android.os.Bundle
+import android.hardware.usb.UsbManager
+import android.widget.Toast
+import android.content.IntentFilter
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Handler
+import java.lang.Thread
+import android.os.Looper
+import java.lang.Runnable
+import java.lang.StringBuffer
+import android.util.Log
 
-import com.example.pl2302_android.uart.UARTSettingInfo;
-
-
-public class MainActivity extends AppCompatActivity {
-    private static boolean bDebugMesg = true;
-    PL2303GMultiLib mSerialMulti;
-    private static final int ReadDataBufferSize = 64;
-    private static final int DeviceIndex1 = 0;
-    private static final int MAX_DEVICE_COUNT = 1;
-    private static final String ACTION_USB_PERMISSION = "com.prolific.pl2300G_multisimpletest.USB_PERMISSION";
-    private static final String NULL = null;
-    private UARTSettingInfo gUARTInfoList[];
-    private int iDeviceCount = 0;
-    private boolean bDeviceOpened[] = new boolean[MAX_DEVICE_COUNT];
-
-    private boolean gThreadStop[] = new boolean[MAX_DEVICE_COUNT];
-    private boolean gRunningReadThread[] = new boolean[MAX_DEVICE_COUNT];
-    private boolean enableFixedCOMPortMode=false;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mSerialMulti = new PL2303GMultiLib((UsbManager) getSystemService(Context.USB_SERVICE),
-                this, ACTION_USB_PERMISSION);
-
-        gUARTInfoList = new UARTSettingInfo[MAX_DEVICE_COUNT];
-
-        for(int i=0;i<MAX_DEVICE_COUNT;i++) {
-            gUARTInfoList[i] = new UARTSettingInfo();
-            gUARTInfoList[i].iPortIndex = i;
-            gThreadStop[i] = false;
-            gRunningReadThread[i] = false;
-            bDeviceOpened[i] = false;
+class MainActivity : AppCompatActivity() {
+    var mSerialMulti: PL2303GMultiLib? = null
+    private lateinit var gUARTInfoList: Array<UARTSettingInfo?>
+    private var iDeviceCount = 0
+    private val bDeviceOpened = BooleanArray(MAX_DEVICE_COUNT)
+    private val gThreadStop = BooleanArray(MAX_DEVICE_COUNT)
+    private val gRunningReadThread = BooleanArray(MAX_DEVICE_COUNT)
+    private val enableFixedCOMPortMode = false
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mSerialMulti = PL2303GMultiLib(
+            getSystemService(USB_SERVICE) as UsbManager,
+            this, ACTION_USB_PERMISSION
+        )
+        gUARTInfoList = arrayOfNulls(MAX_DEVICE_COUNT)
+        for (i in 0 until MAX_DEVICE_COUNT) {
+            gUARTInfoList[i] = UARTSettingInfo()
+            gUARTInfoList[i]!!.iPortIndex = i
+            gThreadStop[i] = false
+            gRunningReadThread[i] = false
+            bDeviceOpened[i] = false
         }
     }
 
-
-
-
-    protected void onDestroy() {
-        DumpMsg("Enter onDestroy");
-        if(mSerialMulti!=null) {
-            for(int i=0;i<MAX_DEVICE_COUNT;i++) {
-                gThreadStop[i] = true;
+    override fun onDestroy() {
+        DumpMsg("Enter onDestroy")
+        if (mSerialMulti != null) {
+            for (i in 0 until MAX_DEVICE_COUNT) {
+                gThreadStop[i] = true
             }
-            if(iDeviceCount>0)
-                unregisterReceiver(PLMultiLibReceiver);
-            mSerialMulti.PL2303Release();
-            mSerialMulti = null;
+            if (iDeviceCount > 0) unregisterReceiver(PLMultiLibReceiver)
+            mSerialMulti!!.PL2303Release()
+            mSerialMulti = null
         }
-        super.onDestroy();
+        super.onDestroy()
     }
 
-    public void onResume() {
-        super.onResume();
-        synchronized (this) {
-            ReSetStatus();
-            iDeviceCount = mSerialMulti.PL2303Enumerate();
-            DelayTime(60);
-            DumpMsg("enumerate Count="+iDeviceCount);
-            if( 0==iDeviceCount ) {
-                Toast.makeText(this, "no more devices found", Toast.LENGTH_SHORT).show();
-                DumpMsg("no more devices found");
+    public override fun onResume() {
+        Log.e("vacax","onResume")
+        super.onResume()
+        synchronized(this) {
+            resetStatus()
+            iDeviceCount = mSerialMulti!!.PL2303Enumerate()
+            delayTime(60)
+            Log.e("vacax","device count=$iDeviceCount")
+            DumpMsg("enumerate Count=$iDeviceCount")
+            if (0 == iDeviceCount) {
+                Toast.makeText(this, "no more devices found", Toast.LENGTH_SHORT).show()
+                DumpMsg("no more devices found")
             } else {
-                openDevice();
-                DumpMsg("DevOpen[0]="+bDeviceOpened[DeviceIndex1]);
-
-                if(!bDeviceOpened[DeviceIndex1]) {
-                    DumpMsg("iDeviceCount(=1)="+iDeviceCount);
-                    if(enableFixedCOMPortMode) {
-                        if(mSerialMulti.PL2303getCOMNumber(0)!=NULL || mSerialMulti.PL2303getCOMNumber(0)!="" ) {
-                            DumpMsg("Button1_COM Number: "+ mSerialMulti.PL2303getCOMNumber(0));
+                openDevice()
+                DumpMsg("DevOpen[0]=" + bDeviceOpened[DeviceIndex1])
+                if (!bDeviceOpened[DeviceIndex1]) {
+                    DumpMsg("iDeviceCount(=1)=$iDeviceCount")
+                    if (enableFixedCOMPortMode) {
+                        if (mSerialMulti!!.PL2303getCOMNumber(0) !== NULL || mSerialMulti!!.PL2303getCOMNumber(
+                                0
+                            ) !== ""
+                        ) {
+                            DumpMsg("Button1_COM Number: " + mSerialMulti!!.PL2303getCOMNumber(0))
                         }
                     }
                 }
-                IntentFilter filter = new IntentFilter();
-                filter.addAction(mSerialMulti.PLUART_MESSAGE);
-                registerReceiver(PLMultiLibReceiver, filter);
-                Toast.makeText(this, "The "+iDeviceCount+" devices are attached", Toast.LENGTH_SHORT).show();
-                DumpMsg("The "+iDeviceCount+" devices are attached");
+                val filter = IntentFilter()
+                filter.addAction(mSerialMulti!!.PLUART_MESSAGE)
+                registerReceiver(PLMultiLibReceiver, filter)
+                Toast.makeText(this, "The $iDeviceCount devices are attached", Toast.LENGTH_SHORT)
+                    .show()
+                DumpMsg("The $iDeviceCount devices are attached")
             }
-
         }
     }
 
-    void openDevice(){
-        OpenUARTDevice(DeviceIndex1);
+    private fun openDevice() {
+        openUARTDevice(DeviceIndex1)
     }
 
-    private final BroadcastReceiver PLMultiLibReceiver = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-            if(intent.getAction().equals(mSerialMulti.PLUART_MESSAGE)){
-                Bundle extras = intent.getExtras();
-                if(extras!=null) {
-                    String str = (String)extras.get(mSerialMulti.PLUART_DETACHED);
-                    DumpMsg("receive data:"+str);
-                    int index = Integer.valueOf(str);
-                    if(DeviceIndex1==index) {
-                        bDeviceOpened[DeviceIndex1] = false;
+    private val PLMultiLibReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            if (intent.action == mSerialMulti!!.PLUART_MESSAGE) {
+                val extras = intent.extras
+                if (extras != null) {
+                    val str=extras.getString(mSerialMulti!!.PLUART_DETACHED)
+                    DumpMsg("receive data:$str")
+                    val index = str?.let { Integer.valueOf(it) }
+                    if (DeviceIndex1 == index) {
+                        bDeviceOpened[DeviceIndex1] = false
                     }
                 }
             }
         }
-    };
-
-
-    private void OpenUARTDevice(int index) {
-        DumpMsg("Enter OpenUARTDevice: "+String.valueOf(index));
-        if(mSerialMulti==null) {
-            DumpMsg("Error: mSerialMulti==null");
-            return;
-        }
-        if(!mSerialMulti.PL2303IsDeviceConnectedByIndex(index)) {
-            DumpMsg("Error: !AP_mSerialMulti.PL2303IsDeviceConnectedByIndex(index)");
-            return;
-        }
-        boolean res;
-        UARTSettingInfo info = gUARTInfoList[index];
-        DumpMsg("UARTSettingInfo: index:"+String.valueOf(info.iPortIndex));
-        res = mSerialMulti.PL2303OpenDevByUARTSetting(index, info.mBaudrate, info.mDataBits, info.mStopBits,
-                info.mParity, info.mFlowControl);
-        if( !res ) {
-            DumpMsg("Error: fail to PL2303OpenDevByUARTSetting");
-            return;
-        }
-        bDeviceOpened[index] = true;
-
-        if(!gRunningReadThread[index]) {
-            UpdateDisplayView(index);
-        }
     }
-    private void UpdateDisplayView(int index) {
-        gThreadStop[index] = false;
-        gRunningReadThread[index] = true;
-        if( DeviceIndex1==index ) {
-            new Thread(ReadLoop1).start();
+
+    private fun openUARTDevice(index: Int) {
+        DumpMsg("Enter OpenUARTDevice: $index")
+        if (mSerialMulti == null) {
+            DumpMsg("Error: mSerialMulti==null")
+            return
+        }
+        if (!mSerialMulti!!.PL2303IsDeviceConnectedByIndex(index)) {
+            DumpMsg("Error: !AP_mSerialMulti.PL2303IsDeviceConnectedByIndex(index)")
+            return
+        }
+        val res: Boolean
+        val info = gUARTInfoList[index]
+        DumpMsg("UARTSettingInfo: index:" + info!!.iPortIndex.toString())
+        res = mSerialMulti!!.PL2303OpenDevByUARTSetting(
+            index, info.mBaudrate, info.mDataBits, info.mStopBits,
+            info.mParity, info.mFlowControl
+        )
+        if (!res) {
+            DumpMsg("Error: fail to PL2303OpenDevByUARTSetting")
+            return
+        }
+        bDeviceOpened[index] = true
+        if (!gRunningReadThread[index]) {
+            updateDisplayView(index)
         }
     }
 
-    private int ReadLen1;
-    private byte[] ReadBuf1 = new byte[ReadDataBufferSize];
-    Handler mHandler1 = new Handler(Looper.myLooper());
-
-    String bytesToHex(byte[] bytes,int len) {
-        char[] hexArray = "0123456789ABCDEF".toCharArray();
-        char[] hexChars = new char[len * 2];
-        for ( int j = 0; j < len; j++ ) {
-            int v = bytes[j] & 0xFF;
-            hexChars[j * 2] = hexArray[v >>> 4];
-            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+    private fun updateDisplayView(index: Int) {
+        gThreadStop[index] = false
+        gRunningReadThread[index] = true
+        if (DeviceIndex1 == index) {
+            Thread(readLoop1).start()
         }
-        return new String(hexChars);
     }
 
-    private final Runnable ReadLoop1 = new Runnable() {
-        public void run() {
-            for (;;) {
-                ReadLen1 = mSerialMulti.PL2303Read(DeviceIndex1, ReadBuf1);
-                if (ReadLen1 > 0) {
-                    DumpMsg("Read  Length : " + ReadLen1);
-                    mHandler1.post(new Runnable() {
-                        public void run() {
-                            StringBuffer sbHex=new StringBuffer();
-                            for (int j = 0; j < ReadLen1; j++) {
-                                sbHex.append((char) (ReadBuf1[j]&0x000000FF));
-                            }
-                            String result=bytesToHex(ReadBuf1,ReadLen1);
-                            Log.e("vaca", "result: "+result);
-                        }
-                    });
-                }
+    private var ReadLen1 = 0
+    private val ReadBuf1 = ByteArray(ReadDataBufferSize)
+    var mHandler1 = Handler(Looper.myLooper()!!)
+    fun bytesToHex(bytes: ByteArray, len: Int): String? {
+        val sb = StringBuffer()
+        for (i in 0 until len) {
+            val hex = Integer.toHexString(bytes[i].toInt() and 0xFF)
+            if (hex.length == 1) {
+                sb.append('0')
+            }
+            sb.append(hex)
+        }
+        return sb.toString()
+    }
 
-                DelayTime(60);
-
-                if (gThreadStop[DeviceIndex1]) {
-                    gRunningReadThread[DeviceIndex1] = false;
-                    return;
+    private val readLoop1 = Runnable {
+        while (true) {
+            ReadLen1 = mSerialMulti!!.PL2303Read(DeviceIndex1, ReadBuf1)
+            if (ReadLen1 > 0) {
+                mHandler1.post {
+                    val result = bytesToHex(ReadBuf1, ReadLen1)
+                    Log.e("vaca", "result: $result")
                 }
             }
-
-        }
-    };
-
-
-
-    private void ReSetStatus(){
-        mSerialMulti.PL2303G_ReSetStatus();
-        if(bDeviceOpened[DeviceIndex1]) {
-            DumpMsg("DeviceIndex1 is open");
-            if(!mSerialMulti.PL2303IsDeviceConnectedByIndex(0)) {
-                DumpMsg("DeviceIndex1: disconnect");
-                bDeviceOpened[DeviceIndex1] = false;
+            delayTime(60)
+            if (gThreadStop[DeviceIndex1]) {
+                gRunningReadThread[DeviceIndex1] = false
+                return@Runnable
             }
         }
     }
 
-
-
-    private void DelayTime(int dwTimeMS) {
-        long StartTime, CheckTime;
-        if(0==dwTimeMS) {
-            Thread.yield();
-            return;
+    private fun resetStatus() {
+        mSerialMulti!!.PL2303G_ReSetStatus()
+        if (bDeviceOpened[DeviceIndex1]) {
+            DumpMsg("DeviceIndex1 is open")
+            if (!mSerialMulti!!.PL2303IsDeviceConnectedByIndex(0)) {
+                DumpMsg("DeviceIndex1: disconnect")
+                bDeviceOpened[DeviceIndex1] = false
+            }
         }
-        StartTime = System.currentTimeMillis();
+    }
+
+    private fun delayTime(dwTimeMS: Int) {
+        val StartTime: Long
+        var CheckTime: Long
+        if (0 == dwTimeMS) {
+            Thread.yield()
+            return
+        }
+        StartTime = System.currentTimeMillis()
         do {
-            CheckTime=System.currentTimeMillis();
-            Thread.yield();
-        } while( (CheckTime-StartTime)<=dwTimeMS);
+            CheckTime = System.currentTimeMillis()
+            Thread.yield()
+        } while (CheckTime - StartTime <= dwTimeMS)
     }
 
-    private static void DumpMsg(Object s) {
-        if(true==bDebugMesg) {
-            Log.d("PL2303MultiUSBApp", "A: " + s.toString());
+    companion object {
+        private const val bDebugMesg = true
+        private const val ReadDataBufferSize = 64
+        private const val DeviceIndex1 = 0
+        private const val MAX_DEVICE_COUNT = 1
+        private const val ACTION_USB_PERMISSION =
+            "com.prolific.pl2300G_multisimpletest.USB_PERMISSION"
+        private val NULL: String? = null
+        private fun DumpMsg(s: Any) {
+            if (true == bDebugMesg) {
+                Log.d("PL2303MultiUSBApp", "A: $s")
+            }
         }
     }
 }
